2b502fef1d452197e49137248d0078051fb52bb5
Rahul Mainwal <57298309+RahulMainwal@users.noreply.github.com>
2024-04-14 23:25:07 +0530

    Create userModel.js

---
+++ RahulMainwal/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  isAvatarImageSet: {
+    type: Boolean,
+    default: false,
+  },
+  avatarImage: {
+    type: String,
+    default: "",
+  },
+  contacts: Array,
+});
+
+module.exports = mongoose.model("ChatnowUsers", userSchema);
